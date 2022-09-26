c9445e2d247b57527f1ab376ced672fcc32c3132
Pallavi 7pute <113908163+pallavi1299@users.noreply.github.com>
2022-09-27 00:22:19 +0530

    upload file

---
+++ multicondition.java
@@ -0,0 +1,32 @@
+package day2;
+
+public class multicondition {
+
+	public static void main(String[] args) {
+		int per=89;
+		if(per<40)
+			System.out.print("fail");
+		else if(per>60 && per<80)
+			System.out.println("1st class");
+		else if(per>40 && per<50)
+			System.out.print("pass");
+		else if(per>80 && per<=100)
+			System.out.println("outstanding");
+		int y=10;
+		if(y==10)
+			System.out.println("true");
+		
+		else if(y>5)
+		{
+			System.out.println("outstanding");
+		System.out.print("outs");
+		}
+		if(y>8)
+	
+			System.out.println("ou");
+		System.out.print("ng");
+	
+
+	}
+
+}
