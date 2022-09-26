19ae2e9749539b50a683fb3b364662b3a5730aae
Pallavi 7pute <113908163+pallavi1299@users.noreply.github.com>
2022-09-27 00:01:56 +0530

    upload file

---
+++ Userinput.java
@@ -0,0 +1,18 @@
+package day2;
+import java.util.Scanner;
+public class Userinput {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter two no:");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		float p=s.nextFloat();
+		s.close();
+		
+		
+		
+
+	}
+
+}
